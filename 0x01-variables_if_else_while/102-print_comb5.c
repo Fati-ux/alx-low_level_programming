a007987f5c862be82c6fa8822c3f92c961226cce
Fati-ux <69637404+Fati-ux@users.noreply.github.com>
2022-04-08 23:43:11 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,13 +16,13 @@ int f = 0;
 while (f < 10)
 {
 	e = 0;
-	while (e < 8)
+	while (e < 9)
 	{
 		d = 0;
-		while (d < 8)
+		while (d < 9)
 		{
 			c = 0;
-			while (c < 8)
+			while (c < 9)
 			{
 				if (!(f == c && e == d))
 				{
