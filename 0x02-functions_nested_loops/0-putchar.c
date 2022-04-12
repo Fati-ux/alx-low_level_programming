714182152cc34ff5cffc6880a2b392ba2d155fce
Fati-ux <69637404+Fati-ux@users.noreply.github.com>
2022-04-12 22:15:51 +0300

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "school.h"
+
+/**
+ * main - Entry point
+ * Return:0
+ */
+
+int main(void)
+{
+char c[7] = "school";
+int i = 0;
+while (i < 6)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
