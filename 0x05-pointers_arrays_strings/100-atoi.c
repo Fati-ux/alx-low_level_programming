796df30c371159fb2bbf09b7e4d7ef73c7e04bb6
Fati-ux <69637404+Fati-ux@users.noreply.github.com>
2022-04-18 23:44:44 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,8 +8,8 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, resp = 0, firstNum;
-
+	int sign = 1, resp = 0 ;
+  int firstNum ;
 	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 	{
 		if (s[firstNum] == '-')
